3b1d7b5e829aa02ddc0e9aa2382d0e21d99bd86a
Maycon Douglas do Nascimento <maycon.douglasn@gmail.com>
2015-07-15 02:13:58 -0300

    fix conflit controller site

--- application/controllers/Site.php
+++ application/controllers/Site.php
@@ -5,7 +5,7 @@ class Site extends CI_Controller
     public function __construct() {
         parent::__construct();
         $this->load->library('session');
-        //$this->load->model('not_model');
+        $this->load->model('not_model');
         
     }
     
